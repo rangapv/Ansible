4a50df90b33d901de9b6b79ef83b775f078092ef
rangapv <rangapv@yahoo.com>
2020-07-06 07:35:53 +0000

    Added gce.py, gce.ini files

---
+++ gcpgce/secrets.py
@@ -0,0 +1,5 @@
+#Replace the GOOGLE_SERVICE_ACCOUNT with the correct service account email ID & Replace the Path to pkey.pem key below
+GCE_PARAMS = ('GOOGLE_SERVICE_ACCOUNT','/Path to pkey.pem')
+#Replace the GOOGLE_PROJECT_ID with the correct GCP Project ID, that's it!
+GCE_KEYWORD_PARAMS = {'project': 'GOOGLE_PROJECT_ID','datacenter':''}
+
